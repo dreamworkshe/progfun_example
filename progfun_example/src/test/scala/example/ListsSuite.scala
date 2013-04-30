9506fe6aef33f15533c3f9e618ee8f82ac6cf5d7
Yao <dreamworkshe@gmail.com>
2013-04-30 11:26:35 -0700

    Finished

--- progfun_example/src/test/scala/example/ListsSuite.scala
+++ progfun_example/src/test/scala/example/ListsSuite.scala
@@ -47,7 +47,7 @@ class ListsSuite extends FunSuite {
    * This allows tests to be written in a more readable manner:
    */
   test("one plus one is three?") {
-    assert(1 + 1 == 3) // This assertion fails! Go ahead and fix it.
+    assert(1 + 1 == 2) // This assertion fails! Go ahead and fix it.
   }
 
 
@@ -72,7 +72,7 @@ class ListsSuite extends FunSuite {
    * We recommend to always use the `===` equality operator when writing tests.
    */
   test("details why one plus one is not three") {
-    assert(1 + 1 === 3) // Fix me, please!
+    assert(1 + 1 === 2) // Fix me, please!
   }
 
 
@@ -120,5 +120,13 @@ class ListsSuite extends FunSuite {
   
   test("max of a few numbers") {
     assert(max(List(3, 7, 2)) === 7)
+  }
+  
+  test("max of 10 numbers") {
+    assert(max(List(5,3,15,34,100,98,76,10,23,101)) === 101)
+  }
+  
+  test("sum of 10 numbers") {
+    assert(sum(List(1,2,3,4,5,6,7,8,9,10)) == 55)
   }
 }
